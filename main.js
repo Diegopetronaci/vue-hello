69e728ea284a1c1ed6f10316affe1a5271e19ca2
Diego <Petronacidiego@gmail.com>
2020-12-14 16:16:21 +0100

    fine esercizio

--- main.js
+++ main.js
@@ -2,18 +2,17 @@
 //Istruzioni:
 //Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 //Bonus: Aggiungere un’immagine presa anch’essa da un data
+
 const name = prompt("inserisci un nome");
 
-let titolo = new Vue ({
-    el: "#titolo",
+
+let container = new Vue ({
+    el: "#container",
     data: {
-        messaggio: "Hey " + name + " piacere di conoscerti",
+        messaggio: "Hey piacere " + name + " di conoscerti!",
+        immagine: "PubG.jpg"
     },
 });
 
-/* const bottone = new Vue({
-    el: "#bottone",
-    data: {
-        messaggio: "click Me",
-    }
-}), */
+
+
